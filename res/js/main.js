b925f4de07e9221b069108a17c4870fc263a69a1
MejrickYJ <mejrick@email.cz>
2023-03-09 19:31:57 +0100

    Update 0.1.0 - Player Movement

--- res/js/main.js
+++ res/js/main.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1600;
 canvas.height = 800;
 
-const speed = 0.4;
+const speed = 0.7;
 
 function clearCanvas() {
   ctx.fillStyle = "white";
@@ -16,53 +16,93 @@ function draw() {
   ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
-function cooldown() {
-  setTimeout;
+function acceleration(){
+  if (player.y + player.height + player.velocityY < canvas.height) {
+    player.velocityY += speed;
+  } else {
+    player.velocityY = 0;
+  }
+}
+
+function movement(){
+  if (keys.a.pressed) player.velocityX = -4;
+  else if (keys.d.pressed) player.velocityX = 4;
+  player.x += player.velocityX;
+
 }
 
 class Player {
   constructor() {
-    this.x = 800;
-    this.y = 25;
-    this.width = 50;
-    this.height = 50;
-    this.velocityX = 10;
-    this.velocityY = 1;
+    this.x = 800;  //pozice na ose X
+    this.y = 25;  //pozice na ose Y
+    this.width = 50;  //šířka hráče
+    this.height = 50; //výška hráče
+    this.velocityX = 0; //rychlost na ose X po které se hráč bude posouvat
+    this.velocityY = 1;  //rychlost na ose Y po které se hráč bude posouvat
   }
   gravity() {
-    player.y += player.velocityY;
-    player.velocityY += speed;
+    player.y += player.velocityY;  //do hráčovy pozice na ose Y se přičíta rychlost na ose Y, způsobuje padání dolů
   }
 }
 
-const player = new Player();
+const player = new Player();  //vytvoření hráče
+
+const keys = { 
+
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+}
+
+  
 
 window.addEventListener("keydown", (event) => {
+  
   switch (event.key) {
     case " ":
-      player.velocityY = -15;
-      console.log("jump");
+       if(player.velocityY === 0) player.velocityY = -15;
+        console.log("jump");
       break;
+      
 
     case "a":
-      ////////////////////////////////
+      keys.a.pressed = true;
       break;
 
     case "d":
-      ////////////////////////////////
+      keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+
+    case "a":
+      keys.a.pressed = false;
+      break;
+
+    case "d":
+      keys.d.pressed = false;
       break;
   }
 });
 
 function gameLoop() {
   window.requestAnimationFrame(gameLoop);
+  player.velocityX = 0;
+  
+  movement();
+
   clearCanvas();
+
   draw();
-  if (player.y + player.height + player.velocityY < canvas.height) {
-    player.velocityY += speed;
-  } else {
-    player.velocityY = 0;
-  }
+
+  acceleration();
+  
   player.gravity();
 }
 

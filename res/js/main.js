657d9923b5a006ba466f06b1b8e9dd03cd29c5e2
MejrickYJ <mejrick@email.cz>
2023-03-28 20:18:29 +0200

    Update 0.1.2 - Collision with platform

--- res/js/main.js
+++ res/js/main.js
@@ -4,36 +4,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1600;
 canvas.height = 800;
 
-const speed = 0.7;
-
-function clearCanvas() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function draw() {
-  ctx.fillStyle = "green";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function acceleration(){
-  if (player.y + player.height + player.velocityY < canvas.height) {
-    player.velocityY += speed;
-  } else {
-    player.velocityY = 0;
-  }
-}
-
-function movement(){
-  if (keys.a.pressed) player.velocityX = -4;
-  else if (keys.d.pressed) player.velocityX = 4;
-  player.x += player.velocityX;
-
-}
-
 class Player {
   constructor() {
-    this.x = 800;  //pozice na ose X
+    this.x = 775;  //pozice na ose X
     this.y = 25;  //pozice na ose Y
     this.width = 50;  //šířka hráče
     this.height = 50; //výška hráče
@@ -45,7 +18,67 @@ class Player {
   }
 }
 
+class Platform {
+  constructor() {
+    this.x = 725;
+    this.y = 75;  
+    this.width = 150;
+    this.height = 50;
+  }
+}
+
 const player = new Player();  //vytvoření hráče
+const platform = new Platform();
+
+let speed = 0.5;
+
+function clearCanvas() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+}
+
+function acceleration(){
+  if (player.y + player.height + player.velocityY < canvas.height) {
+    player.velocityY += speed;
+  } else {
+    player.velocityY = 0;
+  }
+}
+
+function collision(){
+  
+  if(player.y + player.height + player.velocityY >= platform.y && player.x + player.width >= platform.x &&
+     player.y <= platform.y + platform.height && player.x <= platform.x + platform.width){
+    
+    player.velocityY = 0;
+    speed = 0;
+
+  }
+  else{
+    speed = 0.5;
+  }
+
+
+  if(player.x + player.width == platform.x && player.y <= platform.y + platform.height) keys.d.pressed = false;
+  if(player.x == platform.x + platform.width && player.y <= platform.y + platform.height) keys.a.pressed = false;
+}
+
+function movement(){
+  if (keys.a.pressed) player.velocityX = -5;
+  else if (keys.d.pressed) player.velocityX = 5;
+  player.x += player.velocityX;
+
+}
+
+
 
 const keys = { 
 
@@ -95,8 +128,13 @@ function gameLoop() {
   player.velocityX = 0; 
   if(player.x <= 0) keys.a.pressed = false;
   else if(player.x >= 1549) keys.d.pressed = false;
+  else if(player.y <= 0) player.velocityY = 0;
+
+  collision();
 
   movement();
+
+  
 
   clearCanvas();
 
